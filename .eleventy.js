195cad90cb4246be12711adc928ba084134ecef3
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2023-03-27 18:15:48 +0800

    Add configuration and some basic files

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: "_input",
+      output: "_output",
+      includes: "../_includes",
+      layouts: "../_includes/layouts",
+    }
+  }
+}
---
+++ _input/index.md
@@ -0,0 +1,6 @@
+---
+layout: base.html
+title: "Hello World"
+---
+
+# Hello World
