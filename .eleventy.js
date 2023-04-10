71139c89ae7ec737a7e80f12c03b13ae74a3c4e1
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2023-04-10 22:14:12 +0800

    Add custom collections for year/month and year groupings

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@ module.exports = function(eleventyConfig) {
   // Copy the wp-content folder from the exported WordPress site
   eleventyConfig.addPassthroughCopy('wp-content')
 
+  // Add collections that allow groupings by year/month and year
+  eleventyConfig.addCollection('contentByMonth', require('./_utils/collections/contentByDate').contentByMonth)
+  eleventyConfig.addCollection('contentByYear', require('./_utils/collections/contentByDate').contentByYear)
+
   return {
     dir: {
       input: "_input",
