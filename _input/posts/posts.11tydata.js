806f46fdb11ee20e061a2d96bcdbc387d6e8a620
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2023-04-20 01:12:19 +0800

    Add `published` inverse of `eleventyExcludeFromCollections`

--- _input/posts/posts.11tydata.js
+++ _input/posts/posts.11tydata.js
@@ -4,4 +4,8 @@ module.exports = () => ({
   "layout": "post.njk",
   "tags": ["post"],
   "permalink": data => `/${dayjs(data.page.date).format('YYYY/MM')}/${data.page.fileSlug}/`,
+
+  "eleventyComputed": {
+    "eleventyExcludeFromCollections": data => !data.published,
+  },
 })
