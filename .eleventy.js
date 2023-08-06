42b633694e4c5709a939d5fa79fe12046410cc3d
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2023-08-06 23:25:03 +0800

    Add a pagination file that generates pages for collections of months

--- .eleventy.js
+++ .eleventy.js
@@ -12,42 +12,48 @@ module.exports = function (eleventyConfig) {
     // Copy the CSS
     eleventyConfig.addPassthroughCopy('css')
 
-  // Add collections that allow groupings by year/month and year
-  eleventyConfig.addCollection('contentByMonth', require('./_utils/collections/contentByDate').contentByMonth)
-  eleventyConfig.addCollection('contentByYear', require('./_utils/collections/contentByDate').contentByYear)
+    // Add collections that allow groupings by year/month and year
+    eleventyConfig.addCollection('contentByMonth', require('./_utils/collections/contentByDate').contentByMonth)
+    eleventyConfig.addCollection('contentByYear', require('./_utils/collections/contentByDate').contentByYear)
 
-  // Return the collections in use excluding those used for special grouping
-  eleventyConfig.addFilter('regular_tags', (tags) => {
-    const excluded = [
-      'all',
-      'post',
-      'contentByMonth',
-      'contentByYear',
-    ]
+    // Return the collections in use excluding those used for special grouping
+    eleventyConfig.addFilter('regular_tags', (tags) => {
+        const excluded = [
+            'all',
+            'post',
+            'contentByMonth',
+            'contentByYear',
+        ]
 
-    return tags.filter((tag) => !excluded.includes(tag))
-  })
+        return tags.filter((tag) => !excluded.includes(tag))
+    })
 
-  // Keys filter from https://github.com/11ty/eleventy/issues/927#issuecomment-585539708
-  eleventyConfig.addFilter('keys', obj => Object.keys(obj))
+    // Keys filter from https://github.com/11ty/eleventy/issues/927#issuecomment-585539708
+    eleventyConfig.addFilter('keys', obj => Object.keys(obj))
 
-  eleventyConfig.addShortcode('formatted_date', function(date) {
-    return dayjs(date).format('Do MMMM YYYY - dddd')
-  })
+    eleventyConfig.addShortcode('formatted_date', function (date) {
+        return dayjs(date).format('Do MMMM YYYY - dddd')
+    })
 
-  eleventyConfig.setLayoutResolution(false)
+    // Don't support layouts without extensions
+    eleventyConfig.setLayoutResolution(false)
 
     // Posts imported from WordPress had the "post" layout
     eleventyConfig.addLayoutAlias('post', 'post.njk')
 
-  return {
-    dir: {
-      input: "_input",
-      output: "_output",
-      includes: "../_includes",
-      layouts: "../_includes/layouts",
-    },
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-  }
+    // Given a YYYY/MM, return a human-friendly month (e.g. January 2023)
+    eleventyConfig.addFilter('plainEnglishMonth', (formatted_month) => {
+        return dayjs(formatted_month).format('MMMM YYYY')
+    })
+
+    return {
+        dir: {
+            input: "_input",
+            output: "_output",
+            includes: "../_includes",
+            layouts: "../_includes/layouts",
+        },
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+    }
 }
